cf344ef7953bed486a3671adc014e84d8a56deec
Joshua Spinak <jspinak@hotmail.com>
2026-04-23 22:15:35 +0200

    runner: remove unused eslint-disable in OrchestrationLoopBanner.tsx

--- src/components/active-dashboard/OrchestrationLoopBanner.tsx
+++ src/components/active-dashboard/OrchestrationLoopBanner.tsx
@@ -114,7 +114,6 @@ export function OrchestrationLoopBanner() {
     const key = `${sub.running}:${sub.phase}:${sub.currentIteration}`;
     if (key !== lastPhaseRef.current) {
       lastPhaseRef.current = key;
-      // eslint-disable-next-line react-hooks/set-state-in-effect -- subscription-driven refetch on phase change
       fetchStatus();
     }
   }, [subData, fetchStatus]);
